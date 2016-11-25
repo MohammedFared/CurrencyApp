645ccaaa304a54c98a723290014a8eb7a855b3e0
Mohammed Fareed <mohamedfared95@gmail.com>
2016-11-25 21:48:39 +0200

    added splash

---
+++ app/src/main/java/com/currency/android/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.currency.android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Currency Created by Mohammed Fareed on 11/25/2016.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
